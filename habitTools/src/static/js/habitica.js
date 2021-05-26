a30365177bb48ebe1fae3f7fa501be6413a38756
Zohn <hezhihong@phone580.com>
2021-05-26 20:14:40 +0800

    feat(tasks): 0.5.0-添加习惯、每日任务功能

--- habitTools/src/static/js/habitica.js
+++ habitTools/src/static/js/habitica.js
@@ -1,23 +1,45 @@
-const clientValue = "ab029ac0-b53c-451b-829b-1138d283a40c-habitTools";
+const CLIENT_VALUE = "ab029ac0-b53c-451b-829b-1138d283a40c-habitTools";
+const USER_INFO_URL = "https://habitica.com/api/v3/user";
+const TASKS_URL = "https://habitica.com/api/v3/tasks/user";
 
-function getHBUserInfo(userId, userKey, after) {
-    axios.get("https://habitica.com/api/v3/user", {
-        headers: {'x-client': clientValue, 'x-api-user': userId, 'x-api-key': userKey}
+function getHBUserInfo(user, key, after) {
+    axios.get(USER_INFO_URL, {
+        headers: {'x-client': CLIENT_VALUE, 'x-api-user': user, 'x-api-key': key}
     }).then(res => {
         if (res.data.success) {
             after(res.data.data);
         }
     }).catch(err => {
-        console.error("request habitica error: " + err);
+        console.error("getHBUserInfo request habitica error: " + err);
         after(null);
     });
 }
 
-function getHBHabit() {
-    console.info("获取habitica的习惯列表");
-    setTimeout(function () {
-        let message = '';
-        console.info("habitica 返回： " + message);
-        return message;
-    },3000);
+function getHBHabit(user, key, after) {
+    axios.get(TASKS_URL, {
+        headers: {'x-client': CLIENT_VALUE, 'x-api-user': user, 'x-api-key': key}
+    }).then(res => {
+        if (res.data.success) {
+            after(res.data.data);
+        }
+    }).catch(err => {
+        console.error("getHBHabit request habitica error: " + err);
+        after(null);
+    });
+}
+
+function getColorByValue(value) {
+    if (value >= 10) {
+        return "#438EB6";
+    }else if (value >= 5) {
+        return "#339DA8";
+    }else if (value > 0) {
+        return "#229F72";
+    }else if (value === 0) {
+        return "#DDA146";
+    }else if (value > -10) {
+        return "#DF7C39";
+    } else {
+        return "#C64F53";
+    }
 }
